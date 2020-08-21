afd0e40d3cb44816ef50a033cab1ab627d9787a8
516108736 <516108736@qq.com>
2020-08-21 17:13:30 +0800

    add ServeJSON

--- controllers/form.go
+++ controllers/form.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/astaxie/beego"
+	"net/http"
 )
 
 type ReturnMsg struct {
@@ -82,4 +83,17 @@ end:
 	}
 	c.ServeJSON()
 	fmt.Println("END POST", "status", c.Data["json"])
+}
+
+func (c *MainController) ServeJSON() {
+	c.Ctx.Output.Header("Content-Type", "application/json; charset=utf-8")
+	var err error
+	data := c.Data["json"]
+	encoder := json.NewEncoder(c.Ctx.Output.Context.ResponseWriter)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(data)
+	if err != nil {
+		http.Error(c.Ctx.Output.Context.ResponseWriter, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
